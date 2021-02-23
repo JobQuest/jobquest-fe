da7dbe3323dc29574d526ead5cdb1b2655fac6a6
scripka <olga.morgan17@gmail.com>
2021-02-22 17:12:04 -0700

    Add new routes to menu and menuitems

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Button = () => {
   //This is a general button that is responsible for compelting tasks(Accept/Compelete)
   return (
-    <section>
-      Hello
-    </section>
+    <button>
+      Accept
+    </button>
   );
 }
 
