6099b0bfabb7a39cf1fdea9e79acac57fd7e2684
scripka <olga.morgan17@gmail.com>
2021-02-22 17:54:15 -0700

    Add one sample test to cypress and template for apicalls

--- cypress/integration/actions.spec.ts
+++ cypress/integration/actions.spec.ts
@@ -2,8 +2,8 @@
 
 describe('CRA', () => {
   it('shows learn link', function () {
-    cy.visit('http://localhost:3000')
-    cy.get('.App-link').should('be.visible')
-      .and('have.text', 'Learn React')
+    cy.visit('http://localhost:3000/user/profile')
+    cy.get('img').should('be.visible')
+      .and('have.attr', 'src', 'https://www.youngisthan.in/wp-content/uploads/2017/02/Pikachu-407x400.png')
   })
 })
