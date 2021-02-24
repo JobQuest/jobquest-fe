f07a9a40f7f9ff5323958ffbf45b9c1b8a267ed5
scripka <olga.morgan17@gmail.com>
2021-02-24 10:51:37 -0700

    Fix test for Travis CI

--- src/modules/App/App.test.tsx
+++ src/modules/App/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '.';
+import { MemoryRouter, Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App', () => {
+  render(<App />, { wrapper: MemoryRouter });
 });
