bc69f983386a932c3af131f3d3a7b9011bfb293d
scripka <olga.morgan17@gmail.com>
2021-02-25 11:12:49 -0700

    Rebase questslist component

--- src/modules/QuestsList/index.tsx
+++ src/modules/QuestsList/index.tsx
@@ -1,108 +1,61 @@
-import { useEffect, useState } from "react";
-import Grid from "../../ui/Grid/Grid";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import Grid from '../../ui/Grid/Grid'
+import {Link} from 'react-router-dom'
+import userRoutes from '../../routes/user'
 import "./QuestList.scss";
-import userRoutes from "../../routes/user";
+import { CurrentQuests, ComponentPath } from '../../interfaces'
 //this is a component where user can see available quests that can be accepted
-interface QuestList {
-  // id: number
-  // name: string
-  // points: number
-  // description: string
-  // encounters: number
-  // type: string
-  match: object;
-}
 
-type QuestProps = QuestList;
+type QuestProps = CurrentQuests & ComponentPath
 
 const QuestList: React.FC<QuestProps> = (props) => {
-  const { match } = props;
-  const [currentQuest, setCurrentQuest] = useState<object>({});
+  const {match, quests} = props
+  const [currentQuest, setCurrentQuest] = useState<object>({})
 
   return (
-    <section
-      className="page-quest-list"
-      style={
-        {
-          // border: "1px solid black",
-          // height: "90vh",
-          // marginTop: "auto",
-          // marginBottom: "auto",
-          // padding: "1em",
-          // display: "flex",
-          // flexDirection: "column",
-          // justifyContent: "center",
-        }
-      }
-    >
+    <section className=".component-container page-quest-list">
       <h2>Available Quests</h2>
-      <Link to={`/quests/1`}>
-        <div
-          style={{
-            height: "10em",
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            border: "1px solid red",
-            marginTop: "1em",
-          }}
-        >
-          <div>
-            <h2>Name of The Quest 1</h2>
-            <img />
-            <p>Type</p>
-          </div>
-        </div>
-      </Link>
-      <div
-        style={{
-          height: "10em",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          border: "1px solid red",
-          marginTop: "1em",
-        }}
-      >
-        <div>
-          <h2>Name of The Quest 1</h2>
-          <img />
-          <p>Type</p>
-        </div>
-      </div>
-      <div
-        style={{
-          height: "10em",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          border: "1px solid red",
-          marginTop: "1em",
-        }}
-      >
-        <div>
-          <h2>Name of The Quest 2</h2>
-          <img />
-          <p>Type</p>
-        </div>
-      </div>
-      <div
-        style={{
-          height: "10em",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          border: "1px solid red",
-          marginTop: "1em",
-        }}
-      >
-        <div>
-          <h2>Name of The Quest 3</h2>
-          <img />
-          <p>Type</p>
-        </div>
-      </div>
+      <section style= {{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}>
+        {quests.length && 
+          quests.map(quest => 
+            <Link to={`/quests/${quest.id}`}>
+            <div style={{
+              height: "10em",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid red",
+              marginTop: "1em",
+            }}> 
+            <div style={{
+              height: "10em",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid red",
+              marginTop: "1em",
+              }}>
+              <h2 style={{ margin: "0.5em"}}>{quest.name}</h2>
+                <p style={{ margin: "0"}}>{quest.xp} XP</p>
+                <p style={{ margin: "0"}}>Encounters: {quest.encounter_req}</p>
+              </div>
+              <div style={{
+                width: "20em", 
+                display: "flex", 
+                flexDirection: "column",
+                justifyContent: "center"
+                }}>
+                <p style={{ margin: "0"}}>Level {quest.level}</p>
+                <p style={{ margin: "0"}}>{quest.type}</p>
+              </div>
+            </div>
+          </Link> 
+        )}
+      </section>
     </section>
   );
 };
