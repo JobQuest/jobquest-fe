a5312a3f197d1688b636074f4f1cc47acb921575
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-25 14:49:31 -0700

    Adjust Quest log to dynamically render quests

--- src/modules/UserQuestLog/index.tsx
+++ src/modules/UserQuestLog/index.tsx
@@ -3,45 +3,67 @@ import Grid from "../../ui/Grid/Grid";
 import "./QuestLog.scss";
 //this is a component where user can see available quests that can be accepted
 
+interface idObject {
+  id: number;
+}
+
+interface Quest extends idObject {
+  name: string;
+  xp: number;
+  encounter_req: number;
+  type: string;
+  progress: number;
+}
+
+interface CompletedQuests {
+  completedQuests: Array<Quest>;
+}
+
+type UserQuestLogProps = Quest;
+
+const quests = [
+  {
+    id: 1,
+    type: "active",
+    name: "Slap a Goblin",
+    xp: 100,
+    encounter_req: 1,
+    progress: 1,
+  },
+  {
+    id: 2,
+    type: "passive",
+    name: "Slay Ashardalon the Red Dragon",
+    xp: 1000,
+    encounter_req: 3,
+    progress: 3,
+  },
+];
+
 const UserQuestLog = () => {
+  const [completedQuests, setCompletedQuests] = useState<any | null>(null);
+
+  // setCompletedQuests(quests);
+
+  // if (completedQuests) {
   return (
     <section className="page-questlog">
-      <h2>My Quest Log</h2>
-      <div className="divone">
-        <div
-          style={{
-            height: "100%",
-          }}
-        >
-          <h2>My Quest 1</h2>
-          <p>200XP</p>
-          <p>Type</p>
+      <h2 className="quest-log-page-title">
+        My Quest <br />
+        Log
+      </h2>
+      {quests.map((quest: Quest) => (
+        <div className="quest-record">
+          <div className="quest-record-info-wrapper">
+            <h2>{quest.name}</h2>
+            <p>Exp earned:{quest.xp}</p>
+            <p>Type:{quest.type}</p>
+          </div>
         </div>
-      </div>
-      <div className="divone">
-        <div
-          style={{
-            height: "100%",
-          }}
-        >
-          <h2>My Quest 2</h2>
-          <p>200XP</p>
-          <p>Type</p>
-        </div>
-      </div>
-      <div className="divone">
-        <div
-          style={{
-            height: "100%",
-          }}
-        >
-          <h2>My Quest 3</h2>
-          <p>200XP</p>
-          <p>Type</p>
-        </div>
-      </div>
+      ))}
     </section>
   );
+  // }
 };
 
 export default UserQuestLog;
