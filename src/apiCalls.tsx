e8434c721ed9ceef5969cb141e323ad659f56ff2
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-25 16:31:11 -0700

    Add typescript to api calls

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,54 +1,55 @@
-// const getData = (path) => {
-//   return fetch(path).then((response) => {
-//     if (response.ok) {
-//       return response.json();
-//     } else {
-//       throw new Error(
-//         'Sorry we are having difficulty loading this page, please try again later!'
-//       );
-//     }
-//   });
-// };
+const getData = (path: string) => {
+  return fetch(path).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(
+        "Sorry we are having difficulty loading this page, please try again later!"
+      );
+    }
+  });
+};
 
-// const updateData = (path, action, data) => {
-//   return fetch(path, {
-//     method: action,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Accept: 'application/json'
-//     },
-//     body: JSON.stringify(data)
-//   }).then((response) => {
-//     if (response.ok) {
-//       return response;
-//     } else {
-//       throw new Error(
-//         'Sorry we are having difficulty loading this page, please try again later!'
-//       );
-//     }
-//   });
-// };
+const updateData = (path: string, action: string, data: object) => {
+  console.log(data);
+  return fetch(path, {
+    method: action,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(
+        "Sorry we are having difficulty loading this page, please try again later!"
+      );
+    }
+  });
+};
 
-// const baseUrl = '';
+const baseUrl = "http://job-quest-be.herokuapp.com/api/v1";
 
- export const apiCalls = {}
-//   getUser: (email) => {
-//     return getData(`${baseUrl}/users/1`);
-//   },
+export const apiCalls = {
+  getUser: (email: object) => {
+    return updateData(`${baseUrl}/users`, "POST", email);
+  },
 
-//   getQuests: (userId) => {
-//     return getData(`${baseUrl}/events`);
-//   },
+  // getQuests: (userId) => {
+  //   return getData(`${baseUrl}/events`);
+  // },
 
-//   getCurrentQuest: (questId) => {
-//     return getData(`${baseUrl}/events/${id}`);
-//   },
+  // getCurrentQuest: (questId) => {
+  //   return getData(`${baseUrl}/events/${id}`);
+  // },
 
-//   getQuestEncounter: (questId) => {
-//     return getData(`${baseUrl}/events/${id}`);
-//   },
+  // getQuestEncounter: (questId) => {
+  //   return getData(`${baseUrl}/events/${id}`);
+  // },
 
-//   patchUserQuest: () => {
-//     return updateData(`${baseUrl}`);
-//   }
-// };
+  // patchUserQuest: () => {
+  //   return updateData(`${baseUrl}`);
+  // }
+};
