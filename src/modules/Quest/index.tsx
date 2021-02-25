5a550d3bb0694ce9f91582f7550d0f68680cb24e
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-25 08:49:07 -0700

    add classname for background styling

--- src/modules/Quest/index.tsx
+++ src/modules/Quest/index.tsx
@@ -1,72 +1,93 @@
-import { useEffect, useState } from 'react'
-import Grid from '../../ui/Grid/Grid'
-import {useParams} from 'react-router-dom'
-import ActionButton from '../../ui/ActionButton/ActionButton'
+import { useEffect, useState } from "react";
+import Grid from "../../ui/Grid/Grid";
+import { useParams } from "react-router-dom";
+import ActionButton from "../../ui/ActionButton/ActionButton";
+import "./Quest.scss";
 //this is the component that has a current quest that hols all the information
 interface Quest {
-  id: number
-  name: string 
-  points: number
-  description: string
-  encounters: number
-  type: string
+  id: number;
+  name: string;
+  points: number;
+  description: string;
+  encounters: number;
+  type: string;
 }
 
 interface QuestPath {
-  match: object
+  match: object;
 }
 
 interface Encounter {
-  id: number
-  description: string
-  type: string
-  monsterImage: string
+  id: number;
+  description: string;
+  type: string;
+  monsterImage: string;
 }
 
 interface Action {
-  id: number
-  encounter_id: number
-  type: string
-  description: string
-  title: string
+  id: number;
+  encounter_id: number;
+  type: string;
+  description: string;
+  title: string;
 }
 
 // type CurrentQuest = Quest & Encounter & Action & QuestPath
-type CurrentQuest =  QuestPath
+type CurrentQuest = QuestPath;
 const Quest: React.FC<CurrentQuest> = (props) => {
-  const [currentQuest, setCurrentQuest] = useState<object>({})
-  const {match} = props
+  const [currentQuest, setCurrentQuest] = useState<object>({});
+  const { match } = props;
 
-  console.log(match)
+  console.log(match);
   return (
-    <section style={{border: "1px solid black",
-    height: '90vh',
-    marginTop: 'auto',
-    marginBottom: 'auto',
-    padding: '1em'}}>
+    <section
+      className="page-quest"
+      style={
+        {
+          // border: "1px solid black",
+          // height: "90vh",
+          // marginTop: "auto",
+          // marginBottom: "auto",
+          // padding: "1em",
+        }
+      }
+    >
       <div>
         <h2>Name of The Quest</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-          pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. 
-          Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, 
-          in pretium orci vestibulum eget.
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl
+          eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a
+          bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis
+          tincidunt laoreet ex, in pretium orci vestibulum eget.
         </p>
       </div>
-      <div style={{display: "flex", justifyContent:"space-around", marginTop: "1.5em", marginBottom: "1.5em"}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          marginTop: "1.5em",
+          marginBottom: "1.5em",
+        }}
+      >
         <h2>200 XP</h2>
-        <img style={{width: "150px"}} src="http://pixelartmaker.com/art/ab8d3d041eef31b.png"/>
+        <img
+          style={{ width: "150px" }}
+          src="http://pixelartmaker.com/art/ab8d3d041eef31b.png"
+        />
       </div>
       {/* action buttons to fight the moster that moves you to the next encounter */}
-      <div style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'space-around'
-      }}>
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
         <ActionButton />
         <ActionButton />
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Quest;
