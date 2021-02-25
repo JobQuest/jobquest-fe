bc86b5a0d9c014828b586f105f62e80b542323f5
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-25 08:48:26 -0700

    minor fixes

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import Menu from '../Common/Menu'
-import MenuItem from '../Common/MenuItem'
-import Grid from '../../ui/Grid/Grid'
-import Profile from '../User'
-import UserQuestLog from '../UserQuestLog'
-import { useEffect, useState } from 'react'
-import userRoutes from '../../routes/user'
+import React from "react";
+import Menu from "../Common/Menu";
+import MenuItem from "../Common/MenuItem";
+import Grid from "../../ui/Grid/Grid";
+import Profile from "../User";
+import UserQuestLog from "../UserQuestLog";
+import { useEffect, useState } from "react";
+import userRoutes from "../../routes/user";
+import "./HomePage.scss";
 
 interface HomePageProps {
-  children?: object
+  children?: object;
 }
 
 const HomePage: React.FC<HomePageProps> = (props) => {
-  const {children} = props
+  const { children } = props;
   //Homepage component that have a navigation bar and render components based on the url
-  const [buttonRole, setButtonRole] = useState<string>('')
+  const [buttonRole, setButtonRole] = useState<string>("");
 
   return (
-    <Grid column={true} sm={1} md={1}>
+    <section className="homepage">
       <Menu>
         <MenuItem path={userRoutes.profile.path} />
         <MenuItem path={userRoutes.userQuestLog.path} />
         <MenuItem path={userRoutes.availableQuests.path} />
       </Menu>
       {children}
-    </Grid>
+    </section>
   );
-}
+};
 
 export default HomePage;
