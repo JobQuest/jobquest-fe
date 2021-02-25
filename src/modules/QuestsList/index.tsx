188d087fc0c9dea48eeef7ee62cde45ea6c9e3d6
scripka <olga.morgan17@gmail.com>
2021-02-25 11:21:08 -0700

    Rebase questsList and questslist.scss

--- src/modules/QuestsList/index.tsx
+++ src/modules/QuestsList/index.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react'
-import Grid from '../../ui/Grid/Grid'
 import {Link} from 'react-router-dom'
 import userRoutes from '../../routes/user'
 import "./QuestList.scss";
 import { CurrentQuests, ComponentPath } from '../../interfaces'
-//this is a component where user can see available quests that can be accepted
 
 type QuestProps = CurrentQuests & ComponentPath
 
@@ -13,44 +11,21 @@ const QuestList: React.FC<QuestProps> = (props) => {
   const [currentQuest, setCurrentQuest] = useState<object>({})
 
   return (
-    <section className=".component-container page-quest-list">
-      <h2>Available Quests</h2>
-      <section style= {{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-      }}>
+    <section data-cy="quests-list-container" className="component-container page-quest-list">
+      <h2 className="quests-container-title">Available Quests</h2>
+      <section className="quests-list-wrapper">
         {quests.length && 
           quests.map(quest => 
-            <Link to={`/quests/${quest.id}`}>
-            <div style={{
-              height: "10em",
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              border: "1px solid red",
-              marginTop: "1em",
-            }}> 
-            <div style={{
-              height: "10em",
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              border: "1px solid red",
-              marginTop: "1em",
-              }}>
-              <h2 style={{ margin: "0.5em"}}>{quest.name}</h2>
-                <p style={{ margin: "0"}}>{quest.xp} XP</p>
-                <p style={{ margin: "0"}}>Encounters: {quest.encounter_req}</p>
+            <Link data-cy={`quest-${quest.type}`} to={`/quests/${quest.id}`}>
+            <div className="quest-card-wrapper"> 
+              <div className="quest-card-wrapper__left-side">
+              <h2 className="quests-card-title">{quest.name}</h2>
+                <p className="quests-card-details">{quest.xp} XP</p>
+                <p className="quests-card-details">Encounters: {quest.encounter_req}</p>
               </div>
-              <div style={{
-                width: "20em", 
-                display: "flex", 
-                flexDirection: "column",
-                justifyContent: "center"
-                }}>
-                <p style={{ margin: "0"}}>Level {quest.level}</p>
-                <p style={{ margin: "0"}}>{quest.type}</p>
+              <div className="quest-card-wrapper__right-side">
+                <p className="quests-card-details">Level {quest.level}</p>
+                <p className="quests-card-details">{quest.type}</p>
               </div>
             </div>
           </Link> 
