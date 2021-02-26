039f3e6b78ab7ef79167175f9f2539ebfce0da9b
scripka <olga.morgan17@gmail.com>
2021-02-25 22:34:19 -0700

    Apply assets to QuestsList component

--- src/modules/QuestsList/index.tsx
+++ src/modules/QuestsList/index.tsx
@@ -3,12 +3,36 @@ import {Link} from 'react-router-dom'
 import userRoutes from '../../routes/user'
 import "./QuestsList.scss";
 import { CurrentQuests, ComponentPath } from '../../interfaces'
+import questCardActive from '../../assets/Quest Cards/QuestCard_Active.png'
+import questCardActiveH from '../../assets/Quest Cards/QuestCard_Active_Hover.png'
+import questCardPassive from '../../assets/Quest Cards/QuestCard_Passive.png'
+import questCardPassiveH from '../../assets/Quest Cards/QuestCard_Passive_Hover.png'
+import questCardSupportive from '../../assets/Quest Cards/QuestCard_Supportive.png'
+import questCardSupportiveH from '../../assets/Quest Cards/QuestCard_Supportive_Hover.png'
 
 type QuestProps = CurrentQuests & ComponentPath
 
 const QuestList: React.FC<QuestProps> = (props) => {
   const {match, quests} = props
+  const [questTypes, setQuestTypes] = useState<object>({
+    active: false,
+    passive: false,
+    supportive: false
+  })
   const [currentQuest, setCurrentQuest] = useState<object>({})
+
+interface CardTypeObj {
+  active: string[],
+  passive: string[],
+  supportive: string[]
+}
+const cardTypes: CardTypeObj  = {
+  active: [questCardActive, questCardActiveH],
+  passive: [questCardPassive, questCardPassiveH],
+  supportive: [questCardSupportive, questCardSupportiveH]
+} 
+
+// useEffect (() => console.log(typeof questCardPassive),[])
 
   return (
     <section data-cy="quests-list-container" className="page-quest-list">
@@ -16,16 +40,26 @@ const QuestList: React.FC<QuestProps> = (props) => {
       <section className="quests-list-wrapper">
         {quests.length && 
           quests.map(quest => 
-            <Link data-cy={`quest-${quest.type}`} to={`/quests/${quest.id}`}>
-            <div className="quest-card-wrapper"> 
-              <div className="quest-card-wrapper__left-side">
+            <Link 
+              onMouseOver={() => setQuestTypes({...questTypes, [quest.type]: true})}
+              onMouseOut={() => setQuestTypes({...questTypes, [quest.type]: false})}
+              style={{backgroundImage: `url(`+ `${questTypes[quest.type] ? cardTypes[quest.type][1] : cardTypes[quest.type][0]}`+`)`}} 
+              className="quest-card-wrapper" 
+              key={`quest-${quest.id}`} 
+              data-cy={`quest-${quest.type}`} 
+              to={`/quests/${quest.id}`}
+            >
+            <div className="quest-card-inner-wrapper"> 
               <h2 className="quests-card-title">{quest.name}</h2>
-                <p className="quests-card-details">{quest.xp} XP</p>
-                <p className="quests-card-details">Encounters: {quest.encounter_req}</p>
-              </div>
-              <div className="quest-card-wrapper__right-side">
-                <p className="quests-card-details">Level {quest.level}</p>
-                <p className="quests-card-details">{quest.type}</p>
+              <div className="quest-card-inner-box">
+                <div className="quest-card-wrapper__left-side">
+                  <p className="quests-card-details">{quest.xp} XP</p>
+                  <p className="quests-card-details">Encounters: {quest.encounter_req}</p>
+                </div>
+                <div className="quest-card-wrapper__right-side">
+                  <p className="quests-card-details">Level {quest.level}</p>
+                  <p className="quests-card-details">{quest.type}</p>
+                </div>
               </div>
             </div>
           </Link> 
