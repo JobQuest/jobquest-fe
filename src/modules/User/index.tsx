f8e4db01a657a7766f4dbaa1dc88e5425e736a82
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-26 08:50:56 -0700

    Simplify Profile componant by moving functions up to App

--- src/modules/User/index.tsx
+++ src/modules/User/index.tsx
@@ -1,26 +1,31 @@
-import { useEffect, useState } from "react";
+// import { promises } from "fs";
+// import { useEffect, useState } from "react";
+// import { apiCalls } from "../../apiCalls";
+import React from "react";
 import "./Profile.scss";
 
-interface Profile {
-  name: string;
+interface UserProfile {
+  username: string;
   email: string;
   xp: number;
 }
 
-const userOne: Profile = {
-  name: "Tom",
-  email: "user@gmail.com",
-  xp: 1000,
-};
+interface ProfileObject {
+  user: UserProfile;
+}
 
-const Profile = () => {
-  const [user, setUser] = useState<object>({});
+// const userOne: UserProfile = {
+//   username: "Tom",
+//   email: "user@gmail.com",
+//   xp: 1000,
+// };
 
+const Profile: React.FC<ProfileObject> = ({ user }) => {
   return (
     <section className="page-profile">
-      <h1 className="user-page-title">{userOne.name}</h1>
-      <h2 className="user-xp">Current EXP:{userOne.xp}</h2>
-      <h3 className="user-email">E-mail:{userOne.email}</h3>
+      <h1 className="user-page-title">{user.username}</h1>
+      <h2 className="user-xp">Current EXP:{user.xp}</h2>
+      <h3 className="user-email">E-mail:{user.email}</h3>
     </section>
   );
 };
