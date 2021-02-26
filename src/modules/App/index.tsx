17be738a8a468feafd8d74aa5bd9e91d25a80905
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-26 08:49:20 -0700

    Add api call to App for use as props

--- src/modules/App/index.tsx
+++ src/modules/App/index.tsx
@@ -8,12 +8,29 @@ import HomePage from "../HomePage";
 import QuestsList from "../QuestsList";
 import userRoutes from "../../routes/user";
 import { Route, Switch, useParams } from "react-router-dom";
+import { apiCalls } from "../../apiCalls";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState<any | null>(null);
+  const [completedQuests, setCompletedQuests] = useState<any | null>(null);
+
+  const getUserInfo = () => {
+    Promise.resolve(
+      apiCalls.getUser({ email: "olga@example.com" })
+    ).then((response) => setUser(response.data.attributes));
+  };
+
+  useEffect(() => getUserInfo(), []);
+
   return (
     <main className="App">
       <HomePage>
-        <Route exact path={userRoutes.profile.path} component={Profile} />
+        <Route
+          exact
+          path={userRoutes.profile.path}
+          render={() => <Profile user={user} />}
+        />
         <Route
           exact
           path={userRoutes.userQuestLog.path}
