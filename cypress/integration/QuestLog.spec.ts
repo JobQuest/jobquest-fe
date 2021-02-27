07039394f1171beb58766620a2a7f73e3b80ed29
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-27 10:53:07 -0700

    add tests to questlog

--- cypress/integration/QuestLog.spec.ts
+++ cypress/integration/QuestLog.spec.ts
@@ -1,9 +1,18 @@
+import { createYield } from "typescript";
+
 describe("Quest Log", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/user/quests/1");
+    cy.visit("http://localhost:3000/user/quests-log");
   });
 
-  it("Should display a quests details", () => {
-    cy.get();
+  it("Should display a page title", () => {
+    cy.get("body");
+    cy.should("contain", "My Quest Log");
+  });
+
+  it("Should contain a quest record display area", () => {
+    cy.get("[cy-data='quest-record-title']");
+    cy.get("[cy-data='quest-record-xp']");
+    cy.get("[cy-data='quest-record-type']");
   });
 });
