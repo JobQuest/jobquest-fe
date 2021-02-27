4c0d7f1b2b01315bfd45528b0c871c4afb74561a
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-27 11:12:13 -0700

    add error handling for quest log page

--- src/modules/UserQuestLog/index.tsx
+++ src/modules/UserQuestLog/index.tsx
@@ -25,15 +25,24 @@ const UserQuestLog: React.FC<CompletedQuests> = ({ completedQuests }) => {
           Log
         </h2>
         <section className="quest-record-display-bkg">
-          {completedQuests.map((questRecord: Quest) => (
-            <div key={questRecord.id} className="quest-record">
-              <div className="quest-record-info-wrapper">
-                <h2 cy-data="quest-record-title">{questRecord.name}</h2>
-                <p cy-data="quest-record-xp">Exp earned:{questRecord.xp}</p>
-                <p cy-data="quest-record-type">Type:{questRecord.type}</p>
+          {completedQuests &&
+            completedQuests.map((questRecord: Quest) => (
+              <div key={questRecord.id} className="quest-record">
+                <div className="quest-record-info-wrapper">
+                  <h2 cy-data="quest-record-title">{questRecord.name}</h2>
+                  <p cy-data="quest-record-xp">Exp earned:{questRecord.xp}</p>
+                  <p cy-data="quest-record-type">Type:{questRecord.type}</p>
+                </div>
               </div>
+            ))}
+          {!completedQuests && (
+            <div className="quest-record-info-wrapper">
+              <h2>
+                Records of your completed quests will display here. Go complete
+                some quests!
+              </h2>
             </div>
-          ))}
+          )}
         </section>
       </section>
     );
