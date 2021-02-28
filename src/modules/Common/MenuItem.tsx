0a43d3488fb18bf11f1cb69cc87a1401ed7e97dd
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-02-27 17:39:38 -0700

    linting adjustmnets

--- src/modules/Common/MenuItem.tsx
+++ src/modules/Common/MenuItem.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import './MenuItem.scss';
-import { Link } from 'react-router-dom';
-import { MenuItemProps } from '../../interfaces'
-
-
+import React from "react";
+import "./MenuItem.scss";
+import { Link } from "react-router-dom";
+import { MenuItemProps } from "../../interfaces";
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
-  const { path } = props
-//This is a component of individual tab in the menu bar  
-return (
+  const { path } = props;
+  //This is a component of individual tab in the menu bar
+  return (
     <Link to={path}>
-      <aside style={{
-        width: "3em",
-        height: "3em",
-        border: "1px solid purple"
-      }}>
+      <aside
+        style={{
+          width: "3em",
+          height: "3em",
+          border: "1px solid purple",
+        }}
+      >
         Tab
       </aside>
     </Link>
   );
-}
+};
 
 export default MenuItem;
