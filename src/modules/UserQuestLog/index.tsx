62793145c408521982a66e68aafb66e5075d13e3
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-03-01 16:30:08 -0700

    update quest log styling

--- src/modules/UserQuestLog/index.tsx
+++ src/modules/UserQuestLog/index.tsx
@@ -1,7 +1,68 @@
 import "./QuestLog.scss";
-import { idObject, Quest, CompletedQuests } from '../../interfaces'
+import {
+  idObject,
+  Quest,
+  CompletedQuests,
+  QuestEncounterFunctoinality,
+} from "../../interfaces";
 
-const UserQuestLog: React.FC<CompletedQuests> = ({ completedQuests }) => {
+const DUMMYDATA = [
+  {
+    id: 2,
+    name: "Punch the biggest goblin right in the schnoz",
+    xp: 42,
+    encounter_req: 1,
+    type: "active",
+    progress: 1,
+  },
+  {
+    id: 3,
+    name: "Punch the smallest goblin right in the jaw",
+    xp: 24,
+    encounter_req: 1,
+    type: "passive",
+    progress: 1,
+  },
+  {
+    id: 4,
+    name: "Punch the biggest goblin right in the schnoz",
+    xp: 42,
+    encounter_req: 1,
+    type: "active",
+    progress: 1,
+  },
+  {
+    id: 5,
+    name: "Punch the smallest goblin right in the jaw",
+    xp: 24,
+    encounter_req: 1,
+    type: "passive",
+    progress: 1,
+  },
+  {
+    id: 6,
+    name: "Punch the biggest goblin right in the schnoz",
+    xp: 42,
+    encounter_req: 1,
+    type: "active",
+    progress: 1,
+  },
+  {
+    id: 7,
+    name: "Punch the smallest goblin right in the jaw",
+    xp: 24,
+    encounter_req: 1,
+    type: "passive",
+    progress: 1,
+  },
+];
+
+type QuestLog = CompletedQuests | QuestEncounterFunctoinality;
+
+const UserQuestLog: React.FC<QuestLog> = (props) => {
+  const { setActivePage } = props as QuestEncounterFunctoinality;
+  const { completedQuests } = props as CompletedQuests;
+  setActivePage("quest-log");
   if (completedQuests) {
     return (
       <section className="page-questlog">
@@ -20,14 +81,25 @@ const UserQuestLog: React.FC<CompletedQuests> = ({ completedQuests }) => {
                 </div>
               </div>
             ))}
-          {!completedQuests[0] && (
-            <div className="quest-record-info-wrapper">
-              <p className="quest-record-error">
-                Records of your completed quests will display here. Go complete
-                some quests!
-              </p>
-            </div>
-          )}
+          {
+            !completedQuests[0] &&
+              DUMMYDATA.map((questRecord: Quest) => (
+                <div key={questRecord.id} className="quest-record">
+                  <div className="quest-record-info-wrapper">
+                    <h2 cy-data="quest-record-title">{questRecord.name}</h2>
+                    <p cy-data="quest-record-xp">Exp earned:{questRecord.xp}</p>
+                    <p cy-data="quest-record-type">Type:{questRecord.type}</p>
+                  </div>
+                </div>
+              ))
+
+            // <div className="quest-record-info-wrapper">
+            //   <p className="quest-record-error">
+            //     Records of your completed quests will display here. Go complete
+            //     some quests!
+            //   </p>
+            // </div>
+          }
         </section>
       </section>
     );
