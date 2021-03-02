01202d57f36e92be981c3c7c5559d27fa4388ea0
Olga Morgan <olga.morgan17.com>
2021-03-01 23:32:19 -0700

    Add profile hero animation

--- src/modules/User/index.tsx
+++ src/modules/User/index.tsx
@@ -1,6 +1,6 @@
 import { ProfileObject, QuestEncounterFunctoinality } from "../../interfaces";
-
-import heroImage from "../../assets/Extras/Hero.png";
+import SpriteAni from '../Common/SpriteAnimation'
+import hero from "../../assets/Hero/Hero_Idle.png";
 import "./Profile.scss";
 
 type UserProfile = ProfileObject | QuestEncounterFunctoinality;
@@ -8,14 +8,14 @@ type UserProfile = ProfileObject | QuestEncounterFunctoinality;
 const Profile: React.FC<UserProfile> = (props) => {
   const { user } = props as ProfileObject;
   const { setActivePage, ...others } = props as QuestEncounterFunctoinality;
-  setActivePage("profile");
+
   return (
     <section className="page-profile">
       <h1 className="user-page-title">My Profile</h1>
+      <SpriteAni styleClass="profile-hero" path={hero} step={480} lastFrame={-3840} speed={400} width={480} height={188} />
       <h1 data-cy="username" className="user-page-title">
         {user.username}
       </h1>
-      <img alt="hero" src={heroImage} className="hero-image"></img>
       <h2 className="user-xp">Current EXP:{user.xp}</h2>
       <h3 className="user-email">E-mail:{user.email}</h3>
     </section>
