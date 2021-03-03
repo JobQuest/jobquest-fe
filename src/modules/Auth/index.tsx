ed79bcb42086b8bed53caa1c1d9c77794a31db9f
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-03-02 19:43:42 -0700

    add a bit of style to the login button

--- src/modules/Auth/index.tsx
+++ src/modules/Auth/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import './Auth.scss'
-import { useAuth0 } from '@auth0/auth0-react'
-import LoginButton from '../Common/LoginButton'
-import LogoutButton from '../Common/LogoutButton'
-
+import React from "react";
+import "./Auth.scss";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "../Common/LoginButton";
+import LogoutButton from "../Common/LogoutButton";
 
 const Auth = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -13,10 +12,11 @@ const Auth = () => {
 
   return (
     <section className="page-auth">
-      <h1>Start Your Journey Here!</h1>
-      <div>
-      {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
-      </div>
+      <div>{!isAuthenticated ? <LoginButton /> : <LogoutButton />}</div>
+      <h1 className="welcome-text">
+        Start Your <br />
+        Journey Here!
+      </h1>
     </section>
   );
 };
