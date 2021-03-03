2826559f96b1107f682cf38316a1fa72c1b74a8c
Olga Morgan <olga.morgan17.com>
2021-03-03 15:08:34 -0700

    Fix UserProfile cypress tests due to new feature Auth0

--- cypress/integration/QuestLog.spec.ts
+++ cypress/integration/QuestLog.spec.ts
@@ -1,18 +1,28 @@
 import { createYield } from "typescript";
 
 describe("Quest Log", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/user/quests-log");
-  });
+  type Url = string
 
-  it("Should display a page title", () => {
-    cy.get("body");
-    cy.should("contain", "My Quest Log");
-  });
+  it("Users should be able to log in",() => {
+    const url: Url = 'http://localhost:3000/'
+    cy.visit(url)
+    cy.get("[data-cy=login-button]").click()
+    cy.get("[name=username]").type('greatsample44@gmail.com')
+    cy.get("[name=password]").type('greatSample44Password!')
+    cy.get("[name=action]").click()
+  })
 
-  // it("Should contain a quest record display area", () => {
-  //   cy.get("[cy-data='quest-record-title']");
-  //   cy.get("[cy-data='quest-record-xp']");
-  //   cy.get("[cy-data='quest-record-type']");
-  // });
+  it("Each completed quest card should have quest information", () => {
+    cy.get("[data-cy=questslog-tab]").click().url().should("include", "/quests-log");
+    
+    cy.get("[data-cy=quests-log-container]")
+      .should("be.visible")
+      .and("contain", "My Quest Log")
+      .get("[data-cy=quests-log-wrapper]").should("be.visible")
+  });
+  
+  it("Users can log out", () => {
+    cy.get("[data-cy=profile-tab]").click();
+    cy.get('[data-cy=logout-button]').should("contain", "Log Out").click();
+  });
 });
