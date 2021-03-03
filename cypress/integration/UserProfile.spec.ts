8e83e31956580679b08f52e9404825c91a49b465
Olga Morgan <olga.morgan17.com>
2021-03-02 21:25:38 -0700

    Add userProfile test

--- cypress/integration/UserProfile.spec.ts
+++ cypress/integration/UserProfile.spec.ts
@@ -1,36 +1,53 @@
 import { createYield } from "typescript";
 
 describe("User Profile Page", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/user/profile");
+  type Url = string
+  type Name = string
+  type XP = string
+  type Email = string
+
+  it("Login on the website", () => {
+    const url: Url = 'http://localhost:3000/'
+    cy.visit(url)
+    cy.get("[data-cy=login-button]").click()
+    cy.get("[name=username]").type('olga.tolstasheva@gmail.com')
+    cy.get("[name=password]").type('ArtemisIsAmazing1992!')
+    cy.get("[name=action]").click()
   });
 
-  it("Should display a loading message before api calls resolve", () => {
-    cy.contains("Loading");
+  it("Should display a homepage with tabs", () => {
+
+    cy.get("[data-cy=homepage-container]")
+      .get("[data-cy=profile-tab]").should("be.visible")
+      .get("[data-cy=quests-tab]").should("be.visible")
+      .get("[data-cy=questslog-tab]").should("be.visible");
   });
 
-  it("Should display a page title", () => {
-    cy.contains("My Profile");
+  it("Should display a profile page", () => {
+    const email: Email = "olga.tolstasheva@gmail.com"
+    const xp: XP = "600"
+    const username: Name = "olga.tolstasheva@gmail.com"
+
+    cy.get("[data-cy=profile-container]")
+      .should("contain", "My Profile")
+      .and("contain", `${username}`)
+      .and("contain", `Current XP: ${xp}`)
+      .and("contain", `E-mail: ${email}`)
   });
 
-  it("Should display a username", () => {
-    cy.get("[data-cy=username]");
-  });
+  it("Should redirect user to a different pages by clicking on nav tabs", () => {
 
-  it("Should display an image", () => {
-    cy.get(".hero-image");
-  });
-
-  it("Should display user's xp", () => {
-    cy.get(".user-xp");
-  });
-
-  it("Should display user's email", () => {
-    cy.get(".user-email");
+    cy.get("[data-cy=quests-tab]").click().url().should("include", "/quests");
+    cy.get("[data-cy=questslog-tab]").click().url().should("include", "/quests-log");
+    cy.get("[data-cy=profile-tab]").click().url().should("include", "/profile");
   });
 
   it("Should not display info from other elements", () => {
     cy.get("body");
     cy.should("not.contain", "Level");
   });
+
+  it("Should log out user on Log Out", () => {
+    cy.get('[data-cy=logout-button]').should("contain", "Log Out").click().url().should("include", "/");
+  })
 });
