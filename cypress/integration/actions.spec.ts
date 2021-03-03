49768069df4d3006496341caa47db60f55f31427
Olga Morgan <olga.morgan17.com>
2021-03-02 18:20:30 -0700

    Remove test cypress file

--- cypress/integration/actions.spec.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('CRA', () => {
-  it('shows learn link', function () {
-    cy.visit('http://localhost:3000/user/profile')
-    cy.get('img').should('be.visible')
-      .and('have.attr', 'src', 'https://www.youngisthan.in/wp-content/uploads/2017/02/Pikachu-407x400.png')
-  })
-})
