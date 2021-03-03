c79f5c883b47fd7b90c5e62273f61f5cc984d0e8
Olga Morgan <olga.morgan17.com>
2021-03-03 15:09:25 -0700

    Fix UserProfile  cypress tests due to new feature Auth0

--- cypress/integration/UserProfile.spec.ts
+++ cypress/integration/UserProfile.spec.ts
@@ -9,37 +9,53 @@ describe("User Profile Page", () => {
   it("Login on the website", () => {
     const url: Url = 'http://localhost:3000/'
     cy.visit(url)
-    cy.get("[data-cy=login-button]").click()
-    cy.get("[name=username]").type('olga.tolstasheva@gmail.com')
-    cy.get("[name=password]").type('ArtemisIsAmazing1992!')
-    cy.get("[name=action]").click()
+    cy.get("[data-cy=login-button]")
+      .click()
+      .get("[name=username]")
+      .type('greatsample44@gmail.com')
+      .get("[name=password]")
+      .type('greatSample44Password!')
+      .get("[name=action]")
+      .click()
   });
 
   it("Should display a homepage with tabs", () => {
 
     cy.get("[data-cy=homepage-container]")
-      .get("[data-cy=profile-tab]").should("be.visible")
-      .get("[data-cy=quests-tab]").should("be.visible")
-      .get("[data-cy=questslog-tab]").should("be.visible");
+      .get("[data-cy=profile-tab]")
+      .should("be.visible")
+      .get("[data-cy=quests-tab]")
+      .should("be.visible")
+      .get("[data-cy=questslog-tab]")
+      .should("be.visible");
   });
 
   it("Should display a profile page", () => {
-    const email: Email = "olga.tolstasheva@gmail.com"
+    const email: Email = "greatsample44@gmail.com"
     const xp: XP = "600"
-    const username: Name = "olga.tolstasheva@gmail.com"
+    const username: Name = "greatsample44"
 
     cy.get("[data-cy=profile-container]")
       .should("contain", "My Profile")
       .and("contain", `${username}`)
-      .and("contain", `Current XP: ${xp}`)
+      .and("contain", `Current XP:`)
       .and("contain", `E-mail: ${email}`)
   });
 
   it("Should redirect user to a different pages by clicking on nav tabs", () => {
 
-    cy.get("[data-cy=quests-tab]").click().url().should("include", "/quests");
-    cy.get("[data-cy=questslog-tab]").click().url().should("include", "/quests-log");
-    cy.get("[data-cy=profile-tab]").click().url().should("include", "/profile");
+    cy.get("[data-cy=quests-tab]")
+      .click()
+      .url()
+      .should("include", "/quests")
+      .get("[data-cy=questslog-tab]")
+      .click()
+      .url()
+      .should("include", "/quests-log")
+      .get("[data-cy=profile-tab]")
+      .click()
+      .url()
+      .should("include", "/profile");
   });
 
   it("Should not display info from other elements", () => {
@@ -48,6 +64,10 @@ describe("User Profile Page", () => {
   });
 
   it("Should log out user on Log Out", () => {
-    cy.get('[data-cy=logout-button]').should("contain", "Log Out").click().url().should("include", "/");
+    cy.get('[data-cy=logout-button]')
+      .should("contain", "Log Out")
+      .click()
+      .url()
+      .should("include", "/");
   })
 });
