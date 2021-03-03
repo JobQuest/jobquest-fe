6829eed5ac008997f9356e5eaf7df9c3aeb957e4
Olga Morgan <olga.morgan17.com>
2021-03-02 17:32:59 -0700

    Add login and logout buttins

---
+++ src/modules/Common/LogoutButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LogoutButton: React.FC = () => {
+  const { logout } = useAuth0();
+  return (
+    <div 
+      className="logout-button" 
+      style={{cursor: "pointer", zIndex: 99}} 
+      onClick={() => logout({ returnTo: window.location.origin + "/" })}>
+      Log Out
+    </div>
+  );
+};
+
+export default LogoutButton;
