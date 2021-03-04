f85242f1f350e0b550afdd3a5aa7b2aed03ad7b8
Olga Morgan <olga.morgan17.com>
2021-03-03 17:30:28 -0700

    Add style to a logo

--- src/modules/Auth/index.tsx
+++ src/modules/Auth/index.tsx
@@ -3,6 +3,7 @@ import "./Auth.scss";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../Common/LoginButton";
 import LogoutButton from "../Common/LogoutButton";
+import LogoImage from "../../assets/Extras/Logo_jq.png";
 
 const Auth = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -12,6 +13,7 @@ const Auth = () => {
 
 return (
   <section data-cy="auth-page-login" className="page-auth">
+    <img className="logo-first" src={LogoImage} alt="logo-first"/>
     <div>{!isAuthenticated ? <LoginButton /> : <LogoutButton />}</div>
     <h1 className="welcome-text">
       Start Your <br />
